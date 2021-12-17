b239b884c7c45086d4f058d19b53a52925071d41
eminqasimov <eminqasimov@outlook.com>
2021-12-17 20:30:52 +0400

    logo ui

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,14 @@
 import {} from '~/assets/icons'
+import { Logo } from '~/ui'
 
 import styles from './header.module.scss'
 
 export function Header() {
   return (
     <div className={styles.header}>
-      <div className={styles.logo}>Expressbank</div>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
     </div>
   )
 }
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,2 +1,3 @@
 export * from './button'
 export * from './empty-state'
+export * from './logo'
